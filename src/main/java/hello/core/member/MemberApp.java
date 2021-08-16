01f05a46355dd4c3f3ee316cd712bb48eb91ddb8
Jisu Han <weblite@naver.com>
2021-08-17 02:23:36 +0900

    회원관리 개발- DIP , OCP 위반되는 순수 java 코드

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService=new MemberServiceImpl();
+        Member member=new Member(1L,"memberA",Grade.VIP);
+        memberService.join(member);
+
+        Member findMember= memberService.findMember(1L);
+
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member=" + findMember.getName());
+
+
+        // 순수한 자바 코드로 연결여부 확인
+
+
+    }
+}
