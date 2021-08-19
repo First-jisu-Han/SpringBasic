78576bcc11cbb9f4d055a03ff0fa6bd6407f7f18
Jisu Han <weblite@naver.com>
2021-08-20 01:08:06 +0900

    스프링 빈을 java코드말고 XML 기반으로 설정하는 방법

---
+++ src/test/java/hello/core/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.support.GenericXmlContextLoader;
+
+// java code 가 아닌 xml 로 빈설정
+public class XmlAppContext {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+
+    }
+}
