8b423db2560e0d5e793c6a17cf1779ac1b2badec
Jisu Han <weblite@naver.com>
2021-08-22 18:44:32 +0900

    싱글톤 패턴의 주의사항 -무상태 (문제 해결)

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,15 +17,15 @@ public class StatefulServiceTest {
         StatefulService statefulService2=ac.getBean("statefulService",StatefulService.class);
 
         // ThreadA : A사용자 10000원 주문
-        statefulService1.order("UserA",10000);
+        int userAPrice=statefulService1.order("UserA",10000);
         // ThreadB : B사용자 20000원 주문
-        statefulService2.order("UserA",20000);
+        int userBPrice=statefulService2.order("UserA",20000);
 
         // ThreadA : 사용자A 주문금액 조회
-        int price=statefulService1.getPrice();
-        System.out.println("price = " + price);  // 1만원이 아니라 2만원 출력
+        System.out.println("userAPrice = " + userAPrice);  // 기대한 1만원 출력 성공 !
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+
 
     }
 
