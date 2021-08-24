08d775855fb8294d5ce54e0ef43b4f6b31f8327c
Jisu Han <weblite@naver.com>
2021-08-25 00:10:56 +0900

    의존관계 주입 - 수정자 주입 (setter) 으로 의존관계 주입하는 코드

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,4 +11,11 @@ import org.springframework.context.annotation.FilterType;
         basePackages = "hello.core",  // 컴포넌트 스캔의 대상
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class) // @Configuration 이 붙은 것들은 제외시킨다. ( 기존에 @Configuration 을 붙여 직접만든 코드가 있기 때문에)
 )
-public class AutoAppConfig {}
+public class AutoAppConfig {
+    @Bean(name="memoryMemberRepository")
+    public MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+
+}
