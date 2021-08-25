cb0a721af306125c8815aa35a53b75616576df3f
Jisu Han <weblite@naver.com>
2021-08-25 17:47:07 +0900

    생성자 주입으로 테스트 실행(Spring 없이 순수 java 코드로만 )

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -21,10 +21,10 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-
-        return new OrderServiceImpl(
-                memberRepository(),
-                discountPolicy());
+        return null;
+//        return new OrderServiceImpl(
+//                memberRepository(),
+//                discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository(){
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,26 +9,33 @@ import org.springframework.stereotype.Component;
 
 //orderServiceImpl은 두가지의 객체가 필요하다 - discountPolicy와 MemberRepository의 객체
 @Component
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
-    private final DiscountPolicy discountPolicy;
-    private final MemberRepository memberRepository;
 
-    @Autowired   // 생성자 위에 @Autowired 어노테이션을 통해 자동 의존관계 주입.
-    public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy){
-        this.memberRepository=memberRepository;
-        this.discountPolicy=discountPolicy;
+
+    private DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+
+//    @Autowired   // 생성자 위에 @Autowired 어노테이션을 통해 자동 의존관계 주입.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
     }
 
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member=memberRepository.findById(memberId);
-        int discountPrice= discountPolicy.discount(member,itemPrice);
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
 
-        return new Order(memberId,itemName,itemPrice,discountPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
 
     }
+
     // 테스트 할 용도 - @Configuration 을통한 싱글톤 유지 확인 테스트
     public MemberRepository getMemberRepository() {
         return memberRepository;
