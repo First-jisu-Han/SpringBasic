90b48468720de5155b56015ec31be65ea2866536
Jisu Han <weblite@naver.com>
2021-08-29 01:48:50 +0900

    생명주기 콜백 : 스프링컨테이너생성-빈생성-DI-초기화콜백-데이터사용-소멸전콜백-스프링 종료 매커니즘

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac=new AnnotationConfigApplicationContext(LifeCycleConfig.class);  // 스프링 컨테이너 생성
+        NetworkClient client=ac.getBean(NetworkClient.class);  // NetworkClient 타입을 찾아서 스프링 빈 반환, client에 저장
+        ac.close(); //
+    }
+
+    // 스프링 컨테이너 등록시키는 클래스
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev"); // 외부에서 수정자 주입
+            return networkClient;
+        }
+
+
+
+
+    }
+}
