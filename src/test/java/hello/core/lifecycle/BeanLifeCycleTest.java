58f9392884513fb5de0e05407129786a6e7e1546
Jisu Han <weblite@naver.com>
2021-08-30 02:15:51 +0900

    initMethod destroyMethod 를 통한 빈 생명주기 콜백

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
     // 스프링 컨테이너 등록시키는 클래스
     @Configuration
     static class LifeCycleConfig{
-        @Bean
+        @Bean(initMethod = "init",destroyMethod = "close")   // 초기화 + 종료 메서드이름을 적어주면 빈 생명주기 콜백 작동 + 직접 스프링 빈으로 등록하면 종료메서드를 추론할 수 있다. 추론 기능을 사용하기 싫다면 destroyMethod = ""로 해주면된다.
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev"); // 외부에서 수정자 주입
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,26 +1,11 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class NetworkClient implements InitializingBean,DisposableBean {
+public class NetworkClient {
 
-   /* 스프링 빈 생명주기 콜백
-
-    ConfigurableApplicationContext ac=new AnnotationConfigApplicationContext(LifeCycleConfig.class);  // 스프링 컨테이너 생성
-    NetworkClient client=ac.getBean(NetworkClient.class);  // NetworkClient 타입을 찾아서 스프링 빈 반환, client에 저장
-    ac.close();
-
-    ac라는 스프링 컨테이너 생성(LifeCycleConfig.class에 정의된 ) ->
-    client에 NetworkClient 클래스 타입의 스프링 빈을 반환하여 저장 :
-    1. NetworkClient networkClient = new NetworkClient();  client에 저장하는 과정에서 생성자가 호출
-    2. networkClient.setUrl("http://hello-spring.dev");    afterPropertiesSet() 에 의해 초기화 진행 connect(), call("초기화 연결 메세지")
-
-    ac.close(); -> destroy()에 의해 메서드 소멸- 빈사용 중지됨 // disconnect();
-
-    */
     private String url; 
 
     public NetworkClient(){
@@ -44,14 +29,14 @@ public class NetworkClient implements InitializingBean,DisposableBean {
         System.out.println(" close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception{      //초기화를 지원하는 메서드
+
+    public void init() throws Exception{   //초기화 메서드이름
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
-    @Override
-    public void destroy() throws Exception{               // 소멸을 지원하는 메서드
+
+    public void close() throws Exception{   // 종료 메서드이름
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
